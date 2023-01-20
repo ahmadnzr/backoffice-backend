803c82946f2414e36868fca0f75d2332303a3000
Ahmad Nizar <ahmad.rpl001@gmail.com>
2023-01-20 15:18:38 +0700

    setup timezone

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
 const adminRoutes = require("./adminRoutes");
 const userRoutes = require("./userRoutes");
 const { connectDB } = require("./config/dbConnection");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Taipei");
 
 const app = express();
 
